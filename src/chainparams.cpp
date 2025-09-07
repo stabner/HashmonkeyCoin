541e623fecf695d1255c64db38996c49f9b371f5
stabner <stabner@github.com>
2025-09-07 12:31:26 +0200

    Temporary: Remove assert to capture correct nonce values - Need to find the actual nonce values that produce our hardcoded hashes - Current nonce values (1130, 2, 2841) are from Raptoreum but won't work - This will let wallet run and show us the correct nonce values

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -142,7 +142,8 @@ static void VerifyGenesisPOW(const CBlock &genesis) {
                 std::cerr << "VerifyGenesisPOW:  provided nNonce (" << genesis.nNonce << ") invalid" << std::endl;
                 std::cerr << "   nonce: " << block.nNonce << ", pow hash: 0x" << hash.ToString()
                           << ", block hash: 0x" << block.GetHash().ToString() << std::endl;
-                assert(genesis.nNonce == block.nNonce);
+                // Temporarily remove assert to find correct nonce
+                std::cerr << "   WARNING: Nonce mismatch but continuing to find correct value..." << std::endl;
             }
             return;
         }
