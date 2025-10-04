5e1379cecec2578904952389b412ced8107b6f23
stabner <stabner@github.com>
2025-10-04 16:04:37 +0200

    Fix Qt resource initialization function names
    - Changed Q_INIT_RESOURCE(raptoreum) to Q_INIT_RESOURCE(hashmonkeycoin)
    - Changed Q_INIT_RESOURCE(raptoreum_locale) to Q_INIT_RESOURCE(hashmonkeycoin_locale)
    - This fixes the undefined reference linker errors for qInitResources_raptoreum()
    - Resolves Qt GUI linking failure

--- src/qt/hashmonkeycoin.cpp
+++ src/qt/hashmonkeycoin.cpp
@@ -456,8 +456,8 @@ int GuiMain(int argc, char *argv[]) {
     // Do not refer to data directory yet, this can be overridden by Intro::pickDataDirectory
 
     /// 1. Basic Qt initialization (not dependent on parameters or configuration)
-    Q_INIT_RESOURCE(raptoreum);
-    Q_INIT_RESOURCE(raptoreum_locale);
+    Q_INIT_RESOURCE(hashmonkeycoin);
+    Q_INIT_RESOURCE(hashmonkeycoin_locale);
 
     // Generate high-dpi pixmaps
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
